2a36ab1ca0353cc5e5f9dbc5a02e3336e108a5c8
Rupak619 <rupaklondhe619@gmail.com>
2023-11-17 14:55:11 +0530

    new commit

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import "./footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      Copyright &#169; 2023 Rupak Londhe. All right received
+      Copyright &#169; 2023 Rupak Londhe. All right reserved.
     </footer>
   );
 };
